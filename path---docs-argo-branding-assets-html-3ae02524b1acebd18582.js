0028c9e3febdb5b59591c359d6d012ad94106bbb
Alex Collins <alex_collins@intuit.com>
2019-04-05 16:19:48 -0700

    Updates

--- path---docs-argo-branding-assets-html-3ae02524b1acebd18582.js
+++ path---docs-argo-branding-assets-html-3ae02524b1acebd18582.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa235941031e5],{552:function(o,g){o.exports={pathContext:{docHtml:'<h1>Argo Branding Assets</h1>\n<h2>Logo</h2>\n<p><img src="https://github.com/argoproj/argo/blob/master/argo-logo600.png" alt="Argo Logo" title="Argo Logo"></p>',docPath:"argo/branding assets",proj:"argo"}}}});
+webpackJsonp([0xa235941031e5],{551:function(o,g){o.exports={pathContext:{docHtml:'<h1>Argo Branding Assets</h1>\n<h2>Logo</h2>\n<p><img src="https://github.com/argoproj/argo/blob/master/argo-logo600.png" alt="Argo Logo" title="Argo Logo"></p>',docPath:"argo/branding assets",proj:"argo"}}}});
 //# sourceMappingURL=path---docs-argo-branding-assets-html-3ae02524b1acebd18582.js.map
