c810611e291b906303e5c0a9f54c2667550f4cd4
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-05-13 11:50:03 -0700

    Updates

--- path---docs-argo-ui-1663ac517453e7a60ca6.js
+++ path---docs-argo-ui-1663ac517453e7a60ca6.js
@@ -1,2 +1,2 @@
-webpackJsonp([45835261889636],{648:function(o,t){o.exports={pathContext:{docHtml:'<h1>Argo UI</h1>\n<p>Moved to <a href="https://github.com/argoproj/argo-ui">https://github.com/argoproj/argo-ui</a></p>',docPath:"argo/ui/readme",proj:"argo"}}}});
+webpackJsonp([45835261889636],{647:function(o,t){o.exports={pathContext:{docHtml:'<h1>Argo UI</h1>\n<p>Moved to <a href="https://github.com/argoproj/argo-ui">https://github.com/argoproj/argo-ui</a></p>',docPath:"argo/ui/readme",proj:"argo"}}}});
 //# sourceMappingURL=path---docs-argo-ui-1663ac517453e7a60ca6.js.map
